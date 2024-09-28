b625afd5bc0a2ba315f604d089dfac574932db6c
Yebsira Tibebu <yebsiratibebu@Yebsiras-MacBook-Air.local>
2024-09-28 19:40:50 -0400

    My fourth commit

--- Enigma.java
+++ Enigma.java
@@ -27,8 +27,15 @@ public class Enigma{
     
     public String encrypt(String message){
         //TODO
-        for (int i = 0; i < message.length(); i++) { // loop through every charater in message
         
+        for (int i = 0; i < message.length(); i++) { // loop through every charater in message
+        int inx = rotors[0].indexOf(message.charAt(i)); // this find the specific character and the index from the the inner circular array
+        rotors[2].charAt(i); // use the index to find the corresponding character in the outer roto
+        inx = rotors[2].indexOf(message.charAt(inx)); // find the index of the outer rotor
+        rotors[1].charAt(inx); // this gets the middle rotor character at that index
+        int index = rotors[1].indexOf(message.charAt(inx)); // this finds the index of the same character in the middle rotor
+        rotors[2].charAt(index); //this correcspond the middle index with the outer
+        rotate(); // this rotates the circular array after each character is found
         }
 
     
