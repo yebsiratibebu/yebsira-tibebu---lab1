b3bafa13a722a3e08f5e054642cd47ce45d7ef2f
Yebsira Tibebu <yebsiratibebu@Yebsiras-MacBook-Air.local>
2024-11-24 16:48:52 -0600

    finished implementing the EnigmaGUI file after looking over the GUI lecture notes

--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -1,3 +1,11 @@
+import javax.swing.*;
 public class EnigmaGUI {
-    
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setTitle("Enigma GUI lab 4");
+        f.setSize(300, 400);
+        f.setLocation(100, 100);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
 }
